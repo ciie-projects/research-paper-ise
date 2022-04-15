1343a4f1d8b8c68b5454ac7747b3ea55f4c588ca
Venkatesh N P <venki21122@gmail.com>
2022-04-15 13:55:40 +0530

    initial commit

---
+++ src/jsx/layouts/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="copyright">
+        <p>
+          Copyright © Designed &amp; Developed by{" "}
+          <a href="https://ciie.bmsedu.in/developers/" target="_blank"  rel="noreferrer">
+            CIIE-Developers Wing
+          </a>{" "}
+          2021
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
